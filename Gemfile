4db17dd64a962b0d638c12c97061877ff351b7ef
nguyen duc duy <nguyen.duc.duy@framgia.com>
2017-12-26 09:24:25 +0700

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -10,9 +10,9 @@ end
 gem "rails", "~> 5.0.3"
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
-gem 'bootstrap-sass', '3.3.7'
 gem "puma", "~> 3.0"
 # Use SCSS for stylesheets
+gem 'bcrypt',         '3.1.11'
 gem "sass-rails", "~> 5.0"
 # Use Uglifier as compressor for JavaScript assets
 gem "uglifier", ">= 1.3.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  get 'hihihi/home'
 
+  get 'hihihi/help'
 
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help', as: 'helf'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
+  get 'haha_hehe/home'
+
+  get 'haha_hehe/help'
+
+  get 'static_pages/help'
 
   resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root "static_pages#home"
 end
