93041e30389f8e6438d4c3618ddda2153fce0654
nguyen duc duy <nguyen.duc.duy@framgia.com>
2017-12-26 09:24:25 +0700

    Make a basic User model (including secure passwords)

---
+++ app/helpers/hihihi_helper.rb
@@ -0,0 +1,2 @@
+module HihihiHelper
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217001513) do
+ActiveRecord::Schema.define(version: 20171226022201) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20171217001513) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
